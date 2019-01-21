ff3bfbd16d312072ad453568d67debbb3f4c2436
LovelyA72 <a72@luotianyi.pro>
2019-01-21 11:18:19 +0800

    moving to oop!

--- LL Story Maker/Program.cs
+++ LL Story Maker/Program.cs
@@ -16,7 +16,7 @@ namespace LL_Story_Maker
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainUI());
         }
     }
 }
